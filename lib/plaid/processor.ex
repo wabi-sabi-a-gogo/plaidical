c09e3fbadb0b3b6e92795510f5abe11731383a38
Scooter Wadsworth <scooter@earnnest.com>
2020-12-16 15:30:51 -0500

    Do not enforce order on keyword list for Plaid.Processor.get_processor_token/1

--- lib/plaid/processor.ex
+++ lib/plaid/processor.ex
@@ -4,24 +4,29 @@ defmodule Plaid.Processor do
               request_id: nil
 
     @type t :: %__MODULE__{
-      processor_token: String.t(),
-      request_id: String.t()
-    }
+            processor_token: String.t(),
+            request_id: String.t()
+          }
   end
 
   @spec get_processor_token(Keyword.t()) :: {:ok, Plaid.Processor.Token.t()} | {:error, Atom.t()}
-  def get_processor_token([processor: _, access_token: _, account_id: _] = params) do
+  def get_processor_token(params) do
     params = Enum.into(params, %{})
 
-    Plaidical.Application.http_client().request(:post, "/processor/token/create", params)
-    |> case do
-      {:ok, map} ->
-        {:ok, struct(Plaid.Processor.Token, map)}
+    if validate_token_params(params) do
+      Plaidical.Application.http_client().request(:post, "/processor/token/create", params)
+      |> case do
+        {:ok, map} ->
+          {:ok, struct(Plaid.Processor.Token, map)}
 
-      {:error, message} ->
-        {:error, message}
+        {:error, message} ->
+          {:error, message}
+      end
+    else
+      {:error, :invalid_or_missing_params}
     end
   end
 
-  def get_processor_token(_), do: {:error, :invalid_or_missing_params}
+  defp validate_token_params(%{processor: _, access_token: _, account_id: _}), do: true
+  defp validate_token_params(_), do: false
 end
